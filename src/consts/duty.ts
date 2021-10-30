55ceb8bae09e0335823bd93d5ee2bc405ecb6f79
DESKTOP-GDMGT96\liopk <liopku89@gmail.com>
2021-10-30 16:04:14 +0900

    던전 추가

--- src/consts/duty.ts
+++ src/consts/duty.ts
@@ -28,6 +28,311 @@ export const Duties = {
     Paglthan: {
       ko: "파글단",
       en: "Paglth'an"
-    }
+    },
+    MatoyasRelict: {
+      ko: "마토야의 공방",
+      en: "Matoya's Relict"
+    },
+    TheHeroesGauntlet: {
+      ko: "노르브란트",
+      en: "The Heroes' Gauntlet"
+    },
+    AnamnesisAnyder: {
+      ko: "애니드라스 아남네시스",
+      en: "Anamnesis Anyder"
+    },
+    TheGrandCosmos: {
+      ko: "그랑 코스모스",
+      en: "The Grand Cosmos"
+    },
+    AkadaemiaAnyder: {
+      ko: "애니드라스 아남네시스",
+      en: "Akadaemia Anyder"
+    },
+    TheTwining: {
+      ko: "쌍둥이 시르쿠스",
+      en: "The Twining"
+    },
+    Amaurot: {
+      ko: "아모르트",
+      en: "Amaurot"
+    },
+    MtGulg: {
+      ko: "굴그 화산",
+      en: "Mt. Gulg"
+    },
+    MalikahsWell: {
+      ko: "말리카 큰우물",
+      en: "Malikah's Well"
+    },
+    TheQitanaRavel: {
+      ko: "키타나 신굴",
+      en: "The Qitana Ravel"
+    },
+    DohnMheg: {
+      ko: "도느 메그",
+      en: "Dohn Mheg"
+    },
+    HolminsterSwitch: {
+      ko: "홀민스터",
+      en: "Holminster Siwtch"
+    },
+    TheGhimlytDark: {
+      ko: "김리트 황야",
+      en: "The Ghimlyt Dark"
+    },
+    SaintMociannesArboretumHard: {
+      ko: "성 모샨 식물원(어려움)",
+      en: "Saint Mocianne's Arboretum (Hard)"
+    },
+    TheBurn: {
+      ko: "영구 초토지대",
+      en: "The Burn"
+    },
+    TheSwallowsCompass: {
+      ko: "강엔 종묘",
+      en: "The Swallow's Compass"
+    },
+    TheFractalContinuumHard: {
+      ko: "무한연속 박물함(어려움)",
+      en: "The Fractal Continuum (Hard)"
+    },
+    HellsLid: {
+      ko: "지옥뚜껑",
+      en: "Hells' Lid"
+    },
+    TheDrownedCityofSkalla: {
+      ko: "스칼라 유적",
+      en: "The Downed City of Skalla"
+    },
+    TheTempleofTheFist: {
+      ko: "성도산 사원",
+      en: "The Temple of the Fist"
+    },
+    KuganeCastle: {
+      ko: "쿠가네 성",
+      en: "kugane Castle"
+    },
+    AlaMhigo: {
+      ko: "알라미고",
+      en: "Ala Mhigo"
+    },
+    CastrumAbania: {
+      ko: "카스트룸 아바니아",
+      en: "Castrum Abania"
+    },
+    DomaCastle: {
+      ko: "도마 성",
+      en: "Doma Castle"
+    },
+    BardamsMettle: {
+      ko: "바르담 패도",
+      en: "Bardam's Mettle"
+    },
+    ShisuioftheVioletTides: {
+      ko: "시스이 궁",
+      en: "Shishui of the Violet Tides"
+    },
+    TheSirensongSea: {
+      ko: "세이랜 해",
+      en: "The Sirensong Sea"
+    },
+    SohmAlHard: {
+      ko: "솜 알(어려움)",
+      en: "Sohm Al (Hard)"
+    },
+    BaelsarsWall: {
+      ko: "바일사르 장성",
+      en: "Baelsar's Wall"
+    },
+    TheGreatGubalLibraryHard: {
+      ko: "구브라 환상도서관(어려움)",
+      en: "The Great Gubal Library (Hard)"
+    },
+    Xelphatol : {
+      ko: "젤파톨",
+      en: "Xelphatol"
+    },
+    HullbreakerIsleHard : {
+      ko: "난파선의 섬(어려움)",
+      en: "Hubllbreaker Isle (Hard)"
+    },
+    SohrKhai: {
+      ko: "소르 카이",
+      en: "Sohr Khai"
+    },
+    TheLostCityofAmdaporHard: {
+      ko: "암다포르 시가지(어려움)",
+      en: "The Lost City of Amdapor (Hard)"
+    },
+    TheAntitower: {
+      ko: "거꾸로 선 탑",
+      en: "The Antitower"
+    },
+    PharosSiriusHard: {
+      ko: "시리우스 대등대(어려움)",
+      en: "Pharos Sirius (Hard)"
+    },
+    SaintMociannesArboretum: {
+      ko: "성 모샨 식물원",
+      en: "Saint Mocianne's Arboretum"
+    },
+    TheFractalContinuum: {
+      ko: "무한연속 박물함",
+      en: "The Fractal Continuum"
+    },
+    Neverreap: {
+      ko: "거두지 않는 섬",
+      en: "Neverreap"
+    },
+    TheAetherochemicalResearchFacility: {
+      ko: "마과학 연구소",
+      en: "The Aetherochemical Research Facility"
+    },
+    TheGreatGubalLibrary: {
+      ko: "구브라 환상도서관",
+      en: "The Great Gubal Library"
+    },
+    TheVault: {
+      ko: "이슈가르드 교황청",
+      en: "The Vault"
+    },
+    TheAery: {
+      ko: "용의 둥지",
+      en: "The Aery"
+    },
+    SohmAl: {
+      ko: "솜 알",
+      en: "Sohm Al"
+    },
+    TheDuskVigil: {
+      ko: "어스름 요새",
+      en: "The Dusk Vigil"
+    },
+    AmdaporKeepHard: {
+      ko: "옛 암다포르 성(어려움)",
+      en: "Amdapor Keep (Hard)"
+    },
+    TheWanderersPalaceHard: {
+      ko: "방랑자의 궁전(어려움)",
+      en: "The Wanderer's Palace (Hard)"
+    },
+    TheKeeperoftheLake: {
+      ko: "묵약의 탑",
+      en: "The Keeper of the Lake"
+    },
+    TheSunkenTempleofQarnHard: {
+      ko: "카른의 무너진 사원(어려움)",
+      en: "The Sunken Temple of Qarn (Hard)"
+    },
+    SastashaHard: {
+      ko: "사스타샤 침식 동굴(어려움)",
+      en: "Sastasha (Hard)"
+    },
+    Snowcloak: {
+      ko: "얼음외투 대빙벽",
+      en: "Snowcloak"
+    },
+    TheTamTaraDeepcroftHard: {
+      ko: "탐타라 묘소(어려움)",
+      en: "The Tam-Tara Deepcroft (Hard)"
+    },
+    TheStoneVigilHard: {
+      ko: "돌방패 경계초소(어려움)",
+      en: "The Stone Vigil (Hard)"
+    },
+    HullbreakerIsle: {
+      ko: "난파선의 섬",
+      en: "Hullbreaker Isle"
+    },
+    BrayfloxsLongstopHard: {
+      ko: "브레이플록스의 야영지(어려움)",
+      en: "Brayflox's Longstop (Hard)"
+    },
+    HalataliHard: {
+      ko: "할라탈리 수련장(어려움)",
+      en: "Halatali (Hard)"
+    },
+    TheLostCityofAmdapor: {
+      ko: "옛 암다포르 시가지",
+      en: "The Lost City of Amdapor"
+    },
+    HaukkeManorHard: {
+      ko: "하우케타 별궁(어려움)",
+      en: "Haukke Manor (hard)"
+    },
+    CopperbellMinesHard: {
+      ko: "구리종 광산(어려움)",
+      en: "Copperbell Mines (Hard)"
+    },
+    PharosSirius: {
+      ko: "시리우스 대등대",
+      en: "Pharos Sirius"
+    },
+    AmdaporKeep: {
+      ko: "옛 암다포르 성",
+      en: "Amdapor Keep"
+    },
+    ThePraetorium: {
+      ko: "마도성 프라이토리움",
+      en: "The Praetorium"
+    },
+    CastrumMeridianum: {
+      ko: "카스트룸 메리디아눔",
+      en: "Castrum Meridianum"
+    },
+    TheWanderersPalace: {
+      ko: "방랑자의 궁전",
+      en: "The Wanderer's Palace"
+    },
+    TheAurumVale: {
+      ko: "금빛 골짜기",
+      en: "The Aurum Vale"
+    },
+    DzemaelDarkhold: {
+      ko: "제멜 요새",
+      en: "Dzemael Darkhold"
+    },
+    TheStoneVigil: {
+      ko: "돌방패 경계초소",
+      en: "The Stone Vigil"
+    },
+    CuttersCry: {
+      ko: "나무꾼의 비명",
+      en: "Cutter's Cry"
+    },
+    TheSunkenTempleofQarn: {
+      ko: "카른의 무너진 사원",
+      en: "The Sunken Temple of Qarn"
+    },
+    BrayfloxsLongstop: {
+      ko: "브레이플록스의 야영지",
+      en: "Brayflox's Longstop"
+    },
+    HaukkeManor: {
+      ko: "하우케타 별궁",
+      en: "Haukke Manor"
+    },
+    TheThousandMawsofToToRak: {
+      ko: "토토라크 감옥",
+      en: "The Thousand Maws of Toto-Rak"
+    },
+    Halatali: {
+      ko: "할라탈리 수련장",
+      en: "Halatali"
+    },
+    CopperbellMines: {
+      ko: "구리종 광산",
+      en: "Copperbell Mines"
+    },
+    TheTamTaraDeepcroft: {
+      ko: "탐타라 묘소",
+      en: "The Tam-Tara Deepcroft"
+    },
+    Sastasha: {
+      ko: "사스타샤 침식 동굴",
+      en: "Sastasha"
+    }    
   }
+  
 }
